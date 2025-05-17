e04784628e72cd8c2af3d00b26e1c3ee27882bcf
ORIbazak <oricohenbazak@gmail.com>
2025-05-17 12:59:13 +0300

    done before first check #TOMER

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        String STR  ="3*(-1-2*3)";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("equation:");
+        String STR  = sc.nextLine();
+
         calculator calc = new calculator();
         double answer=calc.calc(STR);
         System.out.println(answer);
